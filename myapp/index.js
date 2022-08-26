122cc524d8f1f8f6ed22dc37236aa6d44b1f8f1b
Sourav Paul <111589971+Sourav162306@users.noreply.github.com>
2022-08-26 19:10:00 +0530

    change update book api

--- myapp/index.js
+++ myapp/index.js
@@ -78,7 +78,8 @@ app.post("/books/", async (request, response) => {
 });
 
 //Update Book API
-app.post("/books/", async (request, response) => {
+app.put("/books/:bookId/", async (request, response) => {
+  const { bookId } = request.params;
   const bookDetails = request.body;
   const {
     title,
@@ -93,27 +94,25 @@ app.post("/books/", async (request, response) => {
     price,
     onlineStores,
   } = bookDetails;
-  const addBookQuery = `
-    INSERT INTO
-      book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
-    VALUES
-      (
-        '${title}',
-         ${authorId},
-         ${rating},
-         ${ratingCount},
-         ${reviewCount},
-        '${description}',
-         ${pages},
-        '${dateOfPublication}',
-        '${editionLanguage}',
-         ${price},
-        '${onlineStores}'
-      );`;
-
-  const dbResponse = await db.run(addBookQuery);
-  const bookId = dbResponse.lastID;
-  response.send({ bookId: bookId });
+  const updateBookQuery = `
+    UPDATE
+      book
+    SET
+      title='${title}',
+      author_id=${authorId},
+      rating=${rating},
+      rating_count=${ratingCount},
+      review_count=${reviewCount},
+      description='${description}',
+      pages=${pages},
+      date_of_publication='${dateOfPublication}',
+      edition_language='${editionLanguage}',
+      price=${price},
+      online_stores='${onlineStores}'
+    WHERE
+      book_id = ${bookId};`;
+  await db.run(updateBookQuery);
+  response.send("Book Updated Successfully");
 });
 
 //Delete Book API
